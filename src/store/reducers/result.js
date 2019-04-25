0f5f5be95a978dee855c06a84e97a7dfd163a967
Pedro <pedromedina190@gmail.com>
2019-04-25 10:36:29 -0400

    -used utility.js to get a leaner switch case in reduce
    -used an utility function to get an even cleaner switch case for remove result
    
    -Saw an alternative folder structure, but didn't apply

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,25 +1,19 @@
 import { STORE_RESULT, DELETE_RESULT } from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case STORE_RESULT:
-        return {
-            ...state,
-            results: state.results.concat({id: new Date(), val: action.result})
-        }
-        case DELETE_RESULT:
-        // const id = 2;
-        // const newArray = [...state.results];
-        // state.results.splice(id, 1);
-        const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-        return{
-            ...state,
-            results: updatedArray
-        }
+        case STORE_RESULT: return updateObject(state, {results: state.results.concat({id: new Date(), val: action.result * 2})});
+        case DELETE_RESULT: return deleteResult(state, action);
     };
 
     return state;
